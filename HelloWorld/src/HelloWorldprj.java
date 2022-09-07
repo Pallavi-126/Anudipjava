4b5c9c666d209f85bd2957a88810e82e16bbe3ac
Icon <Icon@LAPTOP-28JK5E5H>
2022-09-07 11:46:58 +0530

    First commit

---
+++ HelloWorld/src/HelloWorldprj.java
@@ -0,0 +1,15 @@
+
+public class HelloWorldprj {
+
+	public static void main(String[] args) {
+		
+		int year=2003;
+		if (year%4==0)
+			System.out.println(year+"=leap year");
+		else
+		System.out.println("This not a leap year");
+
+		    	
+	}
+	
+}
---
+++ HelloWorld/src/TableOf2.java
@@ -0,0 +1,12 @@
+
+public class TableOf2 {
+
+	public static void main(String[] args) {
+		 int num = 2;
+	        for(int i = 1; i <= 10; ++i)
+	        {
+	            System.out.println(num*i);
+	        }
+	}
+
+}
