9bd7d2db6900e89a66030cf51790d4e9ba91c82d
Icon <Icon@LAPTOP-28JK5E5H>
2022-09-07 22:28:51 +0530

    First commit

---
+++ HelloWorld/src/DoWhile.java
@@ -0,0 +1,31 @@
+public class DoWhile
+{
+    public static void main(String...args)
+    {
+    	int i=1;
+    	
+    	while(i<=4)
+    	{
+    		i++;
+    		int j=0;
+    		System.out.println();
+    		System.out.println("Table of "+i);
+    		while(j<=9)
+    		{
+    			j++;
+    			System.out.println(i+"*"+j+"="+i*j);
+    			
+    		}
+            
+    		
+    	}
+    }
+}
+
+
+
+
+
+
+
+
---
+++ WhileLoop/src/NestedWhile.java
@@ -0,0 +1,27 @@
+
+public class NestedWhile
+{
+
+	public static void main(String[] args) {
+		 
+		    
+		    	int i=1;
+		    	
+		    	while(i<=4)
+		    	{
+		    		i++;
+		    		int j=0;
+		    		System.out.println();
+		    		System.out.println("Table of "+i);
+		    		while(j<=9)
+		    		{
+		    			j++;
+		    			System.out.println(i+"*"+j+"="+i*j);
+		    			
+		    		}
+		            
+		    	}	
+	}
+	
+	}
+
