e06aaef5e3861b7aa23d5e4259ab0eca58728050
Pallavi-126 <113004685+Pallavi-126@users.noreply.github.com>
2022-10-01 15:59:08 +0530

    Update ShapeMain.java

--- Lab Test4/src/Calculate/ShapeMain.java
+++ Lab Test4/src/Calculate/ShapeMain.java
@@ -5,9 +5,9 @@ public class ShapeMain {
 	public static void main(String[] args) {
 		Area a=new Area();
 		
-		System.out.println(a.RectangleArea(20,30));          //giving inputs to instances
-		System.out.println(a.CircleArea(5));
-		System.out.println(a.SquareArea(6));
+		System.out.println("Area of Rectangle :"+a.RectangleArea(20,30));          //giving inputs to instance and printing Areas
+		System.out.println("Area of Circle :"+a.CircleArea(5));
+		System.out.println("Area of Square :"+a.SquareArea(6));
 
 	}
 
